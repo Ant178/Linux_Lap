bbae1765195348759887d5f665a7e0b806fc6eca
Juan <juan@escom.com>
2022-12-22 02:11:17 -0600

    hola

---
+++ JS/Hola.js
@@ -0,0 +1,20 @@
+/**
+console.log("Hola mundo");
+document.writeln("\nHola mundo");
+// Variables
+let x=10,y=15;
+let z= "Hola";
+document.writeln((x+y)+"\n"+z);
+*/
+//Cambiar valores desde JS
+//document.getElementById("Juan").innerHTML = "Cambios hechos desde JS"; //EL innerHTML cambia los valores dentro del HTML
+//Lo mismo poner " " a ' '
+//document.write('<h1>Hola Guerrero<\h1>');
+
+//Variables en JS
+let nombre = "Juan";
+//document.write("<h1><\h1>"+ nombre);
+nombre="Sofia";
+//document.write(nombre);
+console.log(nombre);
+const l=
